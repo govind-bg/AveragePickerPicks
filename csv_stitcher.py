f63c0e58b47944bd000a0eee7f0fe73ddea178b8
Govind Ajith Kumar <govind.ajithkumar@berkshiregrey.com>
2022-12-15 09:18:43 -0500

    files to combine all raw csv files

---
+++ csv_stitcher.py
@@ -0,0 +1,16 @@
+# Program to stitch multiple CSV files
+
+import os
+import glob
+import pandas as pd
+
+os.chdir("csv_files/")
+extension = 'csv'
+
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+print('Stitching ', len(all_filenames), ' raw CSV Files ..... ')
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+
+# export to csv
+combined_csv.to_csv("combined_csv.csv", index=False, encoding='utf-8-sig')
